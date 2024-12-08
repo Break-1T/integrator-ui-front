b02492fed1fca3e480c441354f4d1534e4997b24
Taras Krupko <taras.krupko07@gmail.com>
2024-12-08 21:57:37 +0200

    - refactor server.js

--- server.js
+++ server.js
@@ -1,28 +1,41 @@
-var express = require('express');
-var serveStatic = require('serve-static');
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+const serveStatic = require('serve-static');
 
 const port = process.env.PORT || 8081;
+const distDir = path.join(__dirname, 'dist');
 
 const app = express();
-app.use(serveStatic(__dirname + "/dist"));
 
-app.get('*', (req, res) => {
-    // Читаем index.html
-    let indexHTML = fs.readFileSync(path.join(distDir, 'index.html'), 'utf8');
-    
-    // Заменяем плейсхолдер на значение из переменных окружения
-    const integratorUrl = process.env.VITE_INTEGRATOR_API_URL || 'http://localhost:3000'; 
-    indexHTML = indexHTML.replace('%%VITE_INTEGRATOR_API_URL%%', integratorUrl);
-    
-    // Отдаем клиенту уже "настроенный" HTML
-    res.send(indexHTML);
+// Перехватываем запрос к index.html
+app.get('*', (req, res, next) => {
+  // Путь к index.html
+  const indexPath = path.join(distDir, 'index.html');
+
+  // Проверяем, если запрашиваемый файл не index.html, пропускаем дальше
+  if (!req.path.endsWith('/') && !req.path.endsWith('.html') && req.path !== '/') {
+    return next();
+  }
+
+  let indexHTML = fs.readFileSync(indexPath, 'utf8');
+  
+  const integratorUrl = process.env.VITE_INTEGRATOR_API_URL || 'http://localhost:3000'; 
+  console.log('Replacing with:', integratorUrl);
+
+  indexHTML = indexHTML.replace('%%VITE_INTEGRATOR_API_URL%%', integratorUrl);
+  
+  res.send(indexHTML);
+});
+
+// Теперь отдаём статику
+app.use(serveStatic(distDir));
+
+app.get('/health', (req, res) => {
+  res.status(200).json({ status: 'OK' });
+  console.log('Received GET request to /health endpoint');
 });
 
 app.listen(port, () => {
-   console.log(`Server running at port ${port}`);
-});
-
-app.get('/health', (req, res) => {
-    res.status(200).json({ status: 'OK' });
-    console.log('Received GET request to /health endpoint');
+  console.log(`Server running at port ${port}`);
 });
